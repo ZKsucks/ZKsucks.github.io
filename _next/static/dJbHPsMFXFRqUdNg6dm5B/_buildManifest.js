3d71e990ac0163bf7e1d6b5b80349ed8a16d3b73
Yi-Cyuan Chen <emn178@gmail.com>
2024-06-14 15:12:56 +0800

    update

---
+++ _next/static/dJbHPsMFXFRqUdNg6dm5B/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/9857-3fe6d7937254da51.js","static/css/b6047990e780112e.css","static/chunks/pages/index-4a17b50a83758167.js"],"/_error":["static/chunks/pages/_error-eb74a452056fea13.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
